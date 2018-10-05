2e89e85833cecd79f4fcae52ba4b7def111da0f7
Christia T <christiatidwell@bellsouth.net>
2018-10-05 18:41:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    purchases = Purchase.new(params[:item][][:name])
+    purchases = Purchase.new(params[:item][:name])
     @purchases = purchases.collect do |item|
       item
     end
